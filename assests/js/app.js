e8ca4cdb4ec337ba04a214ea52011ab1db40f123
RobinSharma-05 <robin9253370222@gmail.com>
2023-07-04 12:33:52 +0530

    final changes

--- assests/js/app.js
+++ assests/js/app.js
@@ -6,3 +6,26 @@ $(".slick-slider").slick({
     fade: true,
     autoplaySpeed: 2000,
 });
+
+setTimeout(() => {
+    const box = document.getElementById("box");
+
+    box.style.display = "none";
+
+    document.body.classList.remove("overflow-hidden");
+}, 1000);
+
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
